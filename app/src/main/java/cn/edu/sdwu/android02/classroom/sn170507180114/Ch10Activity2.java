eb6822e79d925bcd2c68445735608ccc6cb2cb82
lhm227 <3135692500@qq.com>
2020-04-23 15:41:50 +0800

    练习4.23

--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180114/Ch10Activity2.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180114/Ch10Activity2.java
@@ -3,6 +3,7 @@ package cn.edu.sdwu.android02.classroom.sn170507180114;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
@@ -49,6 +50,15 @@ public class Ch10Activity2 extends AppCompatActivity {
             }else{
                 Toast.makeText(this,"cancel",Toast.LENGTH_SHORT).show();
             }
+        }else if(requestCode==102){
+            //从联系人列表返回的结果
+            if(requestCode==RESULT_OK){
+                //得到联系人的信息（得到联系人的编号，lookup uri）
+                String content=data.getDataString();
+                Toast.makeText(this,content,Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this,"cancel",Toast.LENGTH_SHORT).show();
+            }
         }
     }
     public void web(View view){
@@ -57,4 +67,41 @@ public class Ch10Activity2 extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void contactsList(View view){
+        //查看联系人列表
+        Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("content://contacts/people/"));
+        startActivity(intent);
+    }
+
+    public void contactsDetail(View view){
+        //查看联系人明细
+        Intent intent=new Intent(Intent.ACTION_EDIT);
+        intent.setData(Uri.parse("content://contacts/people/1"));
+        startActivity(intent);
+    }
+
+    public void showMap(View view){
+        //查看地图
+        Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("geo:50.123,7.1434"));
+        startActivity(intent);
+    }
+
+    public void showPhoto(View view){
+        //查看照片
+        Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("geo:50.123,7.1434"));
+        startActivity(intent);
+    }
+
+    public void pickContact(View view){
+        //以子activity的形式，打开联系人列表，让用户选择一个联系人后，返回结果
+        Intent intent=new Intent((Intent.ACTION_PICK));//隐式启动
+        intent.setData(ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent,102);
+    }
+
+    public void implicitStart(View view){
+        Intent intent=new Intent("com.inspur.action2");
+        intent.setData(Uri.parse("abc://inspur.com"));
+        startActivity(intent);
+    }
 }
